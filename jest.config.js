d26c53655da6f1be3166762e809dd70b26fb82e3
Gideon lenkai <gideonlenkai2021@gmail.com>
2025-07-27 19:49:29 +0300

    Move jest.config.js to project root for CodeGrade

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-//fix
-module.exports = {
-  testEnvironment: 'jsdom',
-  roots: ['<rootDir>/src'],
-  moduleFileExtensions: ['js', 'jsx'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  testMatch: ['**/__tests__/**/*.test.js'],
-};
