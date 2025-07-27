92cc6541bc668932019aa7d470689a9a9d3e444a
Gideon lenkai <gideonlenkai2021@gmail.com>
2025-07-27 19:19:49 +0300

    Add Jest config for CodeGrade compatibility

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['js', 'jsx'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testMatch: ['**/__tests__/**/*.test.js'],
+};
